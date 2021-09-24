ff7949a9e608985b87c8a37c9b3506c3712de416
SewookHan <hkihki306@gmail.com>
2021-09-24 18:11:56 +0900

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
+import * as ROUTES from './constants/routes';
 
 export default function App() {
   return (
     <>
       <Router>
         <Switch>
-          <Route path="/" exact>
+          <Route path={ROUTES.HOME} exact>
             <Home />
           </Route>
         </Switch>
